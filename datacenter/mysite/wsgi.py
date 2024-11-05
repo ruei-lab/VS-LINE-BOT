5a112f3fffd4ade3f813d1cf6fe75b0182d71bb4
Ruei <chanruei1228@gmail.com>
2024-11-05 17:07:21 +0800

    Update wsgi.py

--- datacenter/mysite/wsgi.py
+++ datacenter/mysite/wsgi.py
@@ -1,5 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'datacenter.settings')
 application = get_wsgi_application()
